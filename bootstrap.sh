4e31f283a9a20e5c52b17830df123c27aed9fe5e
jondashkyle <contact@jon-kyle.com>
2017-07-11 17:56:35 -0400

    k

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,9 @@ function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude "bootstrap.sh" \
+		--exclude "install.sh" \
+		--exclude "screenshot.png" \
+		--exclude ".gitignore" \
 		--exclude "readme.md" \
 		-avh --no-perms . ~;
 	source ~/.bash_profile;
